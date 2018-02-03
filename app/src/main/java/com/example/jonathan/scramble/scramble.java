a38ad8fd3e870ff73b2560d3ad6ae6e40d70f71c
TheRealJonathanPeers <Jon_athan71@hotmail.com>
2018-02-03 10:04:15 +0100

    forgot to upload project xD

---
+++ app/src/main/java/com/example/jonathan/scramble/scramble.java
@@ -0,0 +1,13 @@
+package com.example.jonathan.scramble;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class scramble extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scramble);
+    }
+}
