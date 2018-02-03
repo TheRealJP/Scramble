ecab0db8b501b87f6042cc04ae0be7ddec514e44
TheRealJonathanPeers <Jon_athan71@hotmail.com>
2018-02-03 12:33:46 +0100

    added scramblestring method

--- app/src/main/java/com/example/jonathan/scramble/scramble.java
+++ app/src/main/java/com/example/jonathan/scramble/scramble.java
@@ -2,6 +2,15 @@ package com.example.jonathan.scramble;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class scramble extends AppCompatActivity {
 
@@ -9,5 +18,26 @@ public class scramble extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scramble);
+
+        Button button = findViewById(R.id.btnScramble);
+        EditText editText = findViewById(R.id.txtWord);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(scrambleString(editText.getText().toString()));
+
+            }
+        });
     }
+
+    private String scrambleString(String s) {
+        List<Character> chars = s.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        Collections.shuffle(chars);
+
+        StringBuilder sb = new StringBuilder();
+        chars.forEach(sb::append);
+        return sb.toString();
+    }
+
 }
